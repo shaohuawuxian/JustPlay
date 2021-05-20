e18f72c1700ea7aff53055653f615411ce6c7069
zhangshao <zhangshao@wecash.net>
2021-05-20 17:08:07 +0800

    提交

---
+++ app/src/main/java/com/zs/justplay/ui/activity/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.zs.justplay.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zs.justplay.databinding.MainActivityBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:MainActivityBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.mainViewPager
+
+    }
+}
---
+++ app/src/main/java/com/zs/justplay/viewmodels/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.zs.justplay.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
